ea7919432c0e35102578f7d258a3f97bc4e21891
Staph. aureus <staph@aosc.io>
2020-06-17 14:51:00 -0400

    I still need to at least provide the template

---
+++ botconfig.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+## This file is used to configure the cu bot.
+
+apiKey = '123456789:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+db = 'init.db'
+superAdmin = (123456789,)
+
+if __name__ == '__main__':
+    print('DO NOT EXECUTE ME!')
